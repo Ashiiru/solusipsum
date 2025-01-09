92f76a775527cc98c6a59472d0f9cd4fa414e9f3
Ashiiru <afonsomonteirosilva17@outlook.com>
2025-01-09 16:18:10 +0000

    ye

--- Scripts/elevador_descer.gd
+++ Scripts/elevador_descer.gd
@@ -5,9 +5,7 @@ var has_played: bool = false  # To ensure the sequence runs only once
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player") and not has_played:
-		
 		animation_player.play("elevador_baixo")
-		
 		print("Elevador Desceu")
 		has_played = true  # Prevent re-triggering
 		
--- Scripts/trigger_anim_door_2.gd
+++ Scripts/trigger_anim_door_2.gd
@@ -1,14 +1,10 @@
 extends Area3D
 
-@onready var animation_player = $"../Trigger_Anim_Door/AnimationPlayer"
+@onready var animation_player = $"../Trigger_Anim_Door/AnimationPlayer2"
 
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-<<<<<<< Updated upstream:Scripts/trigger_anim_door_2.gd
 		animation_player.play("door_open")
-=======
-		animation_player.play("AnimationPlayer").play("door_open")
->>>>>>> Stashed changes:Scenes/trigger_anim_door_2.gd
 		print("Porta Abriu Baixo")
