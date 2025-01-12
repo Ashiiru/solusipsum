13a18bb2c7a96846e3394cc5144a90967ef6d52a
Ipu57 <121640471+Ipu57@users.noreply.github.com>
2025-01-12 19:05:59 +0000

    Sons Level 1

--- Scripts/abrir_fechar_porta.gd
+++ Scripts/abrir_fechar_porta.gd
@@ -1,14 +1,61 @@
 extends Area3D
 
-@onready var animation_player = $AnimationPlayer
+@export var animation_player_path: NodePath  # Path to the AnimationPlayer node
+@export var open_sound_path: NodePath  # Path to the AudioStreamPlayer3D for opening sound
+@export var close_sound_path: NodePath  # Path to the AudioStreamPlayer3D for closing sound
+@export var player_group: String = "Player"  # Group name to check for the player
+@export var open_animation: String = "door_open"  # Name of the "open" animation
+@export var close_animation: String = "door_closed"  # Name of the "close" animation
 
+var animation_player: AnimationPlayer
+var open_sound: AudioStreamPlayer3D
+var close_sound: AudioStreamPlayer3D
+var is_playing_animation: bool = false  # Tracks if an animation is currently playing
+var pending_animation: String = ""  # Stores any pending animation
+
+func _ready() -> void:
+	# Cache the AnimationPlayer node
+	if animation_player_path:
+		animation_player = get_node(animation_player_path)
+		animation_player.animation_finished.connect(_on_animation_finished)
+	else:
+		push_error("AnimationPlayer path is not set in the Inspector!")
+	
+	# Cache the AudioStreamPlayer3D nodes
+	if open_sound_path:
+		open_sound = get_node(open_sound_path)
+	else:
+		push_error("Open sound path is not set!")
+	
+	if close_sound_path:
+		close_sound = get_node(close_sound_path)
+	else:
+		push_error("Close sound path is not set!")
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		animation_player.play("door_open")
-		print("Porta Abriu")
+	if body.is_in_group(player_group) and not is_playing_animation:
+		play_animation(open_animation)
+		if open_sound:
+			open_sound.play()
+	elif body.is_in_group(player_group):
+		pending_animation = open_animation
 
 func _on_body_exited(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		animation_player.play("door_closed")
-		print("Porta Abriu Baixo")
+	if body.is_in_group(player_group) and not is_playing_animation:
+		play_animation(close_animation)
+		if close_sound:
+			close_sound.play()
+	elif body.is_in_group(player_group):
+		pending_animation = close_animation
+
+func play_animation(animation_name: String) -> void:
+	if animation_player and not is_playing_animation:
+		is_playing_animation = true
+		animation_player.play(animation_name)
+
+func _on_animation_finished(anim_name: String) -> void:
+	is_playing_animation = false
+	if pending_animation != "":
+		var next_animation = pending_animation
+		pending_animation = ""
+		play_animation(next_animation)
--- Scripts/elevador_descer.gd
+++ Scripts/elevador_descer.gd
@@ -1,13 +1,43 @@
 extends Area3D
 
 @onready var animation_player = $"../Trigger_Anim_Door/AnimationPlayer2"
-var has_played: bool = false  # To ensure the sequence runs only once
+@onready var move_sound = $"../../Corredor_Afonso_v3/elevador/elevador move"  # Moving sound
+@onready var end_sound = $"../../Corredor_Afonso_v3/elevador/elevador sino"  # Ending sound
+var has_played: bool = false  # Ensure the sequence runs only once
+
+func _ready() -> void:
+	# Debugging logs
+	print("Elevator system initialized.")
+	if not animation_player:
+		print("Error: AnimationPlayer not found!")
+	if not move_sound:
+		print("Error: Move sound 'elevador move' not found!")
+	if not end_sound:
+		print("Error: End sound 'elevador sino' not found!")
+
+	# Connect animation_finished signal
+	if animation_player:
+		animation_player.animation_finished.connect(_on_animation_finished)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player") and not has_played:
-		animation_player.play("elevador_baixo")
-		print("Elevador Desceu")
-		has_played = true  # Prevent re-triggering
+		if animation_player:
+			animation_player.play("elevador_baixo")
+			print("Playing animation 'elevador_baixo'")
+		if move_sound:
+			move_sound.play()
+			print("Playing move sound 'elevador move'")
+		has_played = true
+
+func _on_animation_finished(animation_name: String) -> void:
+	print("Animation finished:", animation_name)
+	if animation_name == "elevador_baixo":  # Ensure it's the correct animation
+		if end_sound:
+			end_sound.play()
+			print("Playing end sound 'elevador sino'")
+
+
+
 		
 
 		
